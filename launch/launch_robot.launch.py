da93b717bd52c641d057c1139d98da7f808a5350
Josh Newans <josh.newans@gmail.com>
2022-10-01 12:53:57 +1000

    Fix rsp being in sim mode on real robot.

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
     
