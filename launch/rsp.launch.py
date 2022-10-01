1b11a37e0c842a3fa569bf2f9c02891812c0fd39
Josh Newans <josh.newans@gmail.com>
2022-10-01 12:51:05 +1000

    Unbreak sim mode

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('articubot_one'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml()
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control])
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
