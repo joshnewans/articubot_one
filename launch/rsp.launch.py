68459dd14fc20c86f5554340fb01f81e6c6f6527
neochaos42 <125705606+neochaos42@users.noreply.github.com>
2023-03-18 18:50:41 -0500

    Update rsp.launch.py
    Fixes rsp.py to load robot description as a string

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -6,7 +6,7 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
+from launch_ros.parameter_descriptions import ParameterValue
 import xacro
 
 
@@ -17,20 +17,21 @@ def generate_launch_description():
     use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('articubot_one'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    pkg_path = os.path.join(get_package_share_directory('wubot'))
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml()
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
-    
+    robot_description_config = ParameterValue(Command(
+        ['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time]), value_type=str)
+
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config,
+              'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
     )
-
 
     # Launch!
     return LaunchDescription([
