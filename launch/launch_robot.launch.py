8175893af2acdabe3e2ed47601dfc3dca60687d8
Josh Newans <josh.newans@gmail.com>
2023-03-25 09:16:40 +1100

    Changes for humble.

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -74,7 +74,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -57,13 +57,13 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
