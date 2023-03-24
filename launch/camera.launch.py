cd1eb0f503012a65552fd088cdbd9cf8ac133cbc
Josh Newans <josh.newans@gmail.com>
2023-03-25 07:43:34 +1100

    Add camera and tracker changes.

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -13,8 +13,10 @@ def generate_launch_description():
             package='v4l2_camera',
             executable='v4l2_camera_node',
             output='screen',
+            namespace='camera',
             parameters=[{
                 'image_size': [640,480],
+                'time_per_frame': [1, 6],
                 'camera_frame_id': 'camera_link_optical'
                 }]
     )
