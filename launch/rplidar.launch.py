128189e06622f42a838f4a48d38fcb11a8b33be8
Glorie Ramazani <glorieramazani@gmail.com>
2023-10-10 13:39:33 -0700

    updates to use DiffDriveArduinoHardware

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -2,19 +2,30 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
+from launch.substitutions import LaunchConfiguration
 
+def generate_launch_description():
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+    frame_id = LaunchConfiguration('frame_id', default='laser_frame')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Sensitivity')
     return LaunchDescription([
 
         Node(
             package='rplidar_ros',
-            executable='rplidar_composition',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-                'frame_id': 'laser_frame',
-                'angle_compensate': True,
-                'scan_mode': 'Standard'
-            }]
+            executable='rplidar_node',
+            name='rplidar_node',
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port,
+                         'serial_baudrate': serial_baudrate,
+                         'frame_id': frame_id,
+                         'inverted': inverted,
+                         'angle_compensate': angle_compensate,
+                           'scan_mode': scan_mode
+                         }],
+            output='screen'
         )
     ])
